a47ec35c7ddf0e5f684c301a0f4dff1aceb63948
Bruno Tomé <ibrunotome@gmail.com>
2017-02-19 16:13:45 -0300

    Configuração do webpack-dev-server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
+const gulp = require('gulp');
 const elixir = require('laravel-elixir');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.config');
 const webpackDevConfig = require('./webpack.dev.config');
 
@@ -10,14 +13,28 @@ Elixir.webpack.config.module.loaders = [];
 Elixir.webpack.mergeConfig(webpackConfig);
 Elixir.webpack.mergeConfig(webpackDevConfig);
 
-console.log(Elixir.webpack);
+gulp.task('webpack-dev-server', () => {
+    let config = Elixir.webpack.config;
+    new WebpackDevServer(webpack(config), {
+        watchOptions: {
+            pool: true,
+            aggregateTimeout: 300,
+        },
+        publicPath: config.output.publicPath,
+        noInfo: true,
+        stats: {
+            colors: true
+        }
+    }).listen(8080, "localhost", function () {
+        console.log("Bundling project...")
+    })
+});
+elixir(mix => {
+    mix.sass('./resources/assets/admin/sass/admin.scss')
+        .copy('./node_modules/materialize-css/fonts/roboto', './public/fonts/roboto');
 
-// elixir(mix => {
-//     mix.sass('./resources/assets/admin/sass/admin.scss')
-//         .copy('./node_modules/materialize-css/fonts/roboto', './public/fonts/roboto');
-//
-//     mix.browserSync({
-//         host: 'localhost',
-//         proxy: 'http://localhost:8000'
-//     });
-// });
+    mix.browserSync({
+        host: 'localhost',
+        proxy: 'http://localhost:8000'
+    });
+});
