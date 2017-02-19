febd8ccc2ea94d670656868b56c5a2b6d945d402
Bruno Tomé <ibrunotome@gmail.com>
2017-02-19 16:02:31 -0300

    Configuração do webpack em gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const elixir = require('laravel-elixir');
+const webpackConfig = require('./webpack.config');
+const webpackDevConfig = require('./webpack.dev.config');
 
 require('laravel-elixir-vue');
+require('laravel-elixir-webpack-official');
 
-elixir(mix => {
-    mix.sass('./resources/assets/admin/sass/admin.scss')
-        .copy('./node_modules/materialize-css/fonts/roboto', './public/fonts/roboto');
+Elixir.webpack.config.module.loaders = [];
 
-    mix.browserSync({
-        host: 'localhost',
-        proxy: 'http://localhost:8000'
-    });
-});
+Elixir.webpack.mergeConfig(webpackConfig);
+Elixir.webpack.mergeConfig(webpackDevConfig);
+
+console.log(Elixir.webpack);
+
+// elixir(mix => {
+//     mix.sass('./resources/assets/admin/sass/admin.scss')
+//         .copy('./node_modules/materialize-css/fonts/roboto', './public/fonts/roboto');
+//
+//     mix.browserSync({
+//         host: 'localhost',
+//         proxy: 'http://localhost:8000'
+//     });
+// });
