146cadaa20268ede90672470a1893f1d55b766ed
Bruno Tomé <ibrunotome@gmail.com>
2017-02-19 14:45:17 -0300

    Prefixo admin para rotas de autenticação.

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', function () {
     return redirect()->route('admin.home');
 });
 
+Auth::routes();
+
 Route::group([
     'prefix'     => 'admin',
-    'middleware' => 'can:access-admin',
     'as'         => 'admin.'
 ], function () {
-    Route::get('/home', 'HomeController@index')->name('home');
+    Auth::routes();
+    Route::group(['middleware' => 'can:access-admin'], function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
 });
